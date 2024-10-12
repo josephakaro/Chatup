a94c57d428e2d739a7ef1841a03fd5ddd53eb0b4
Joseph Akaro <josephmmakaro@gmail.com>
2024-10-12 17:53:18 +0300

    del: App.js Change made, stash required

--- chatup-react/src/App.tsx
+++ chatup-react/src/App.tsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to ChatUp</h1>
+      <input type="text" placeholder="Message" />
+      <button>Send</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
