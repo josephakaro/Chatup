292ac9ed3804000683a84888d61ca06fc1f682ee
Destiny-Kay <destinykevogo@gmail.com>
2024-10-12 07:29:33 +0300

    Add:restructured application to use yarn and vite

---
+++ chatup-react/src/pages/Home.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+    return(
+    <div>This is the home page</div>
+)
+}
---
+++ chatup-react/src/routes.tsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    }
+]
+)
